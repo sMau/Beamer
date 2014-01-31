3851a42b1013655a540d849b1ded81922b85767f
Samuel Schüppen <samuel.schueppen@rwth-aachen.de>
2014-01-31 14:45:14 +0100

    started with MessageJoin Implementation

--- BeamerLibrary/src/de/netprojectev/networking/MessageJoin.java
+++ BeamerLibrary/src/de/netprojectev/networking/MessageJoin.java
@@ -5,10 +5,31 @@ import io.netty.channel.ChannelInboundHandlerAdapter;
 
 public class MessageJoin extends ChannelInboundHandlerAdapter {
 
+	private byte state = WAITING;
+	
+	public static final byte WAITING = 0;
+	public static final byte READ_OPCODE = 1;
+	public static final byte READ_DATA_LENGTH = 2;
+	
+	
 	@Override
 	public void channelRead(ChannelHandlerContext ctx, Object msg)
 			throws Exception {
 		
+		switch (state) {
+		case WAITING:
+			
+			break;
+		case READ_OPCODE:
+			
+			break;
+		case READ_DATA_LENGTH:
+			
+			break;
+		default:
+			
+			break;
+		}
 		
 		
 		super.channelRead(ctx, msg);
